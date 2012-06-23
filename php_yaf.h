1e829a72a073d6f222133d893d5cb7214896a8e3
Xinchen Hui <laruence@gmail.com>
2012-06-23 17:50:32 +0800

    Sync with pecl-svn (2.1.17-stable)

--- php_yaf.h
+++ php_yaf.h
@@ -14,7 +14,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_yaf.h 325762 2012-05-21 06:17:39Z laruence $ */
+/* $Id: php_yaf.h 326315 2012-06-23 09:45:27Z laruence $ */
 
 #ifndef PHP_YAF_H
 #define PHP_YAF_H
@@ -41,7 +41,7 @@ extern zend_module_entry yaf_module_entry;
 #define YAF_G(v) (yaf_globals.v)
 #endif
 
-#define YAF_VERSION 					"2.1.17-dev"
+#define YAF_VERSION 					"2.2.0-dev"
 
 #define YAF_STARTUP_FUNCTION(module)   	ZEND_MINIT_FUNCTION(yaf_##module)
 #define YAF_RINIT_FUNCTION(modle)		ZEND_RINIT_FUNCTION(yaf_##module)
