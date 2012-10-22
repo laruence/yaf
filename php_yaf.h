1a2e900c609c1be45acf381ee80911d6ed8dd86d
Xinchen Hui <laruence@gmail.com>
2012-10-22 11:20:21 +0800

    prepare for 2.2.5

--- php_yaf.h
+++ php_yaf.h
@@ -14,7 +14,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_yaf.h 327925 2012-10-08 02:28:54Z laruence $ */
+/* $Id: php_yaf.h 328091 2012-10-22 03:19:58Z laruence $ */
 
 #ifndef PHP_YAF_H
 #define PHP_YAF_H
@@ -41,7 +41,7 @@ extern zend_module_entry yaf_module_entry;
 #define YAF_G(v) (yaf_globals.v)
 #endif
 
-#define YAF_VERSION 					"2.2.5-dev"
+#define YAF_VERSION 					"2.2.5"
 
 #define YAF_STARTUP_FUNCTION(module)   	ZEND_MINIT_FUNCTION(yaf_##module)
 #define YAF_RINIT_FUNCTION(module)		ZEND_RINIT_FUNCTION(yaf_##module)
