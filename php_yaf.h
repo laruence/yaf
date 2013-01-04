e4bd8bfc3db3f233f6f59f1127e1bee8cd27b8b6
Xinchen Hui <laruence@gmail.com>
2013-01-04 18:54:02 +0800

    back to dev (2.2.10-dev)

--- php_yaf.h
+++ php_yaf.h
@@ -14,7 +14,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: php_yaf.h 328979 2013-01-04 10:50:55Z laruence $ */
+/* $Id: php_yaf.h 328981 2013-01-04 10:53:59Z laruence $ */
 
 #ifndef PHP_YAF_H
 #define PHP_YAF_H
@@ -41,7 +41,7 @@ extern zend_module_entry yaf_module_entry;
 #define YAF_G(v) (yaf_globals.v)
 #endif
 
-#define YAF_VERSION 					"2.2.9"
+#define YAF_VERSION 					"2.2.10-dev"
 
 #define YAF_STARTUP_FUNCTION(module)   	ZEND_MINIT_FUNCTION(yaf_##module)
 #define YAF_RINIT_FUNCTION(module)		ZEND_RINIT_FUNCTION(yaf_##module)
