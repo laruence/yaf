9305e52e359925b51e883a1427083c6a521c8c4d
Xinchen Hui <laruence@gmail.com>
2015-06-01 16:01:39 +0800

    clean up

--- yaf_dispatcher.h
+++ yaf_dispatcher.h
@@ -17,31 +17,31 @@
 #ifndef PHP_YAF_DISPATCHER_H
 #define PHP_YAF_DISPATCHER_H
 
-#define YAF_DISPATCHER_PROPERTY_NAME_REQUEST	"_request"
-#define YAF_DISPATCHER_PROPERTY_NAME_VIEW		"_view"
-#define YAF_DISPATCHER_PROPERTY_NAME_ROUTER		"_router"
-#define YAF_DISPATCHER_PROPERTY_NAME_INSTANCE	"_instance"
-#define	YAF_DISPATCHER_PROPERTY_NAME_RENDER		"_auto_render"
-#define	YAF_DISPATCHER_PROPERTY_NAME_RETURN		"_return_response"
-#define	YAF_DISPATCHER_PROPERTY_NAME_FLUSH		"_instantly_flush"
-#define	YAF_DISPATCHER_PROPERTY_NAME_ARGS		"_invoke_args"
+#define YAF_DISPATCHER_PROPERTY_NAME_REQUEST    "_request"
+#define YAF_DISPATCHER_PROPERTY_NAME_VIEW       "_view"
+#define YAF_DISPATCHER_PROPERTY_NAME_ROUTER     "_router"
+#define YAF_DISPATCHER_PROPERTY_NAME_INSTANCE   "_instance"
+#define YAF_DISPATCHER_PROPERTY_NAME_RENDER     "_auto_render"
+#define YAF_DISPATCHER_PROPERTY_NAME_RETURN     "_return_response"
+#define YAF_DISPATCHER_PROPERTY_NAME_FLUSH      "_instantly_flush"
+#define YAF_DISPATCHER_PROPERTY_NAME_ARGS       "_invoke_args"
 
-#define YAF_DISPATCHER_PROPERTY_NAME_MODULE		"_default_module"
-#define YAF_DISPATCHER_PROPERTY_NAME_CONTROLLER	"_default_controller"
-#define YAF_DISPATCHER_PROPERTY_NAME_ACTION		"_default_action"
+#define YAF_DISPATCHER_PROPERTY_NAME_MODULE     "_default_module"
+#define YAF_DISPATCHER_PROPERTY_NAME_CONTROLLER "_default_controller"
+#define YAF_DISPATCHER_PROPERTY_NAME_ACTION     "_default_action"
 
-#define YAF_ERROR_CONTROLLER					"Error"
-#define YAF_ERROR_ACTION						"error"
+#define YAF_ERROR_CONTROLLER                    "Error"
+#define YAF_ERROR_ACTION                        "error"
 
-#define YAF_DISPATCHER_PROPERTY_NAME_PLUGINS		"_plugins"
+#define YAF_DISPATCHER_PROPERTY_NAME_PLUGINS    "_plugins"
 
-#define YAF_PLUGIN_HOOK_ROUTESTARTUP				"routerstartup"
-#define YAF_PLUGIN_HOOK_ROUTESHUTDOWN 			"routershutdown"
-#define YAF_PLUGIN_HOOK_LOOPSTARTUP				"dispatchloopstartup"
-#define YAF_PLUGIN_HOOK_PREDISPATCH 				"predispatch"
-#define YAF_PLUGIN_HOOK_POSTDISPATCH				"postdispatch"
-#define YAF_PLUGIN_HOOK_LOOPSHUTDOWN				"dispatchloopshutdown"
-#define YAF_PLUGIN_HOOK_PRERESPONSE				"preresponse"
+#define YAF_PLUGIN_HOOK_ROUTESTARTUP            "routerstartup"
+#define YAF_PLUGIN_HOOK_ROUTESHUTDOWN           "routershutdown"
+#define YAF_PLUGIN_HOOK_LOOPSTARTUP             "dispatchloopstartup"
+#define YAF_PLUGIN_HOOK_PREDISPATCH             "predispatch"
+#define YAF_PLUGIN_HOOK_POSTDISPATCH            "postdispatch"
+#define YAF_PLUGIN_HOOK_LOOPSHUTDOWN            "dispatchloopshutdown"
+#define YAF_PLUGIN_HOOK_PRERESPONSE             "preresponse"
 
 #define YAF_PLUGIN_HANDLE(p, n, request, response) \
 	do { \
