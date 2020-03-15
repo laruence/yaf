41ce966e8d944d0b69949195cf886fe02ec55a45
Xinchen Hui <laruence@gmail.com>
2020-03-15 19:54:55 +0800

    back to dev

--- php_yaf.h
+++ php_yaf.h
@@ -44,7 +44,7 @@ extern zend_module_entry yaf_module_entry;
 # define ZEND_ACC_DTOR	0x0
 #endif
 
-#define PHP_YAF_VERSION 					"3.1.0"
+#define PHP_YAF_VERSION 					"3.1.1-dev"
 
 #define YAF_STARTUP_FUNCTION(module)   	ZEND_MINIT_FUNCTION(yaf_##module)
 #define YAF_RINIT_FUNCTION(module)		ZEND_RINIT_FUNCTION(yaf_##module)
