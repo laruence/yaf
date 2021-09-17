70360065eadf89c55b669d4e6d812d68e6895f01
sky <wangbizhou@didiglobl.com>
2021-09-17 22:51:22 +0800

    edit format_name default value, fix set controller  when using default controller

--- yaf_request.c
+++ yaf_request.c
@@ -1052,7 +1052,7 @@ PHP_METHOD(yaf_request, getActionName) {
 */
 PHP_METHOD(yaf_request, setModuleName) {
 	zend_string *module;
-	zend_bool format_name = 0;
+	zend_bool format_name = 1;
 	yaf_request_object *request = Z_YAFREQUESTOBJ_P(getThis());
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS(), "S|b", &module, &format_name) == FAILURE) {
@@ -1065,8 +1065,8 @@ PHP_METHOD(yaf_request, setModuleName) {
 		} else {
 			if (request->module) {
 				zend_string_release(request->module);
+				request->module = zend_string_copy(module);
 			}
-			request->module = zend_string_copy(module);
 		}
 	}
 
@@ -1078,7 +1078,7 @@ PHP_METHOD(yaf_request, setModuleName) {
 */
 PHP_METHOD(yaf_request, setControllerName) {
 	zend_string *controller;
-	zend_bool format_name = 1;
+	zend_bool format_name = 0;
 	yaf_request_object *request = Z_YAFREQUESTOBJ_P(getThis());
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS(), "S|b", &controller, &format_name) == FAILURE) {
@@ -1091,8 +1091,8 @@ PHP_METHOD(yaf_request, setControllerName) {
 		} else {
 			if (request->controller) {
 				zend_string_release(request->controller);
-				request->controller = zend_string_copy(controller);
 			}
+			request->controller = zend_string_copy(controller);
 		}
 	}
 
